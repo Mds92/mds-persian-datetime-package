57401d75c63d4eb7ccd40d3704de1a578207a493
Mohammad Dayyan <Mds92@users.noreply.github.com>
2020-10-27 11:11:32 +0330

    add test

--- basicTests.test.js
+++ basicTests.test.js
@@ -2,6 +2,19 @@
 /// <reference types="ts-jest" />
 Object.defineProperty(exports, "__esModule", { value: true });
 var mds_persian_datetime_1 = require("./mds.persian.datetime");
+describe('convertToPersianDate', function () {
+    it('1363-01-01 without time', function () {
+        var persianDate1 = mds_persian_datetime_1.Mds.PersianDateTime.fromPersianDate(1363, 1, 1);
+        var date1 = new Date(1984, 2, 21);
+        console.log(date1);
+        expect(persianDate1.getTime()).toBe(date1.getTime());
+    });
+    it('1363-01-01', function () {
+        var persianDate1 = mds_persian_datetime_1.Mds.PersianDateTime.fromPersianDate(1363, 1, 1);
+        var persianDate2 = new mds_persian_datetime_1.Mds.PersianDateTime(new Date(1984, 2, 21));
+        expect(persianDate1.getTime()).toBe(persianDate2.getTime());
+    });
+});
 describe('getDifference', function () {
     it('1 Year difference should return 365 days and 0 hours', function () {
         var testObj = mds_persian_datetime_1.Mds.PersianDateTime.fromPersianDate(1398, 1, 1);
@@ -22,7 +35,6 @@ describe('getDifference', function () {
         var mehr = new mds_persian_datetime_1.Mds.PersianDateTime(new Date('9/23/2019'));
         var shahrivar = new mds_persian_datetime_1.Mds.PersianDateTime(new Date('8/23/2019'));
         var res = mehr.getDifference(shahrivar);
-        //console.log(res);
         expect(res.days).toBe(31);
         expect(res.hours).toBe(0);
     });
@@ -30,7 +42,6 @@ describe('getDifference', function () {
         var mehr = mds_persian_datetime_1.Mds.PersianDateTime.fromPersianDate(1398, 7, 1);
         var shahrivar = mds_persian_datetime_1.Mds.PersianDateTime.fromPersianDate(1398, 6, 31);
         var res = mehr.getDifference(shahrivar);
-        //console.log(res);
         expect(res.days).toBe(1);
         expect(res.hours).toBe(0);
     });
@@ -38,7 +49,6 @@ describe('getDifference', function () {
         var farvardin = new mds_persian_datetime_1.Mds.PersianDateTime(new Date('3/21/2019'));
         var esfand = new mds_persian_datetime_1.Mds.PersianDateTime(new Date('2/20/2019'));
         var res = farvardin.getDifference(esfand);
-        //console.log(res);
         expect(res.days).toBe(29);
         expect(res.hours).toBe(0);
     });
@@ -46,8 +56,7 @@ describe('getDifference', function () {
 describe('getLongNumber', function () {
     it('getLongNumber should return correct time', function () {
         var persianDateTime = mds_persian_datetime_1.Mds.PersianDateTime.fromPersianDateTime(1398, 2, 1, 22, 14, 30, 0);
-        var persianDateTimeLongNumber = persianDateTime.getLongNumber();
-        expect(persianDateTimeLongNumber).toBe(13980201221430);
+        expect(persianDateTime.getLongNumber()).toBe(13980201221430);
     });
 });
 //# sourceMappingURL=basicTests.test.js.map
--- basicTests.test.ts
+++ basicTests.test.ts
@@ -2,6 +2,21 @@
 
 import { Mds } from "./mds.persian.datetime";
 
+describe('convertToPersianDate', () => {
+	it('1363-01-01 without time 1', function () {
+		let persianDate1 = Mds.PersianDateTime.fromPersianDate(1363, 1, 1);
+		let date1 = new Date(1984,2,21);
+		console.log(date1);
+		expect(persianDate1.getTime()).toBe(date1.getTime());
+	});
+
+	it('1363-01-01 without time 2', function () {
+		let persianDate1 = Mds.PersianDateTime.fromPersianDate(1363, 1, 1);
+		let persianDate2 = new Mds.PersianDateTime(new Date(1984, 2, 21));
+		expect(persianDate1.getTime()).toBe(persianDate2.getTime());
+	});
+});
+
 describe('getDifference', () => {
 	it('1 Year difference should return 365 days and 0 hours', function() {
 		var testObj = Mds.PersianDateTime.fromPersianDate(1398, 1, 1);
@@ -23,9 +38,7 @@ describe('getDifference', () => {
 	it('1/7 - 1-6 Should Return 31 days and 0 hours', function() {
 		var mehr = new Mds.PersianDateTime(new Date('9/23/2019'));
 		let shahrivar = new Mds.PersianDateTime(new Date('8/23/2019'));
-
 		let res: Mds.PersianDateTimeSpan2 = mehr.getDifference(shahrivar);
-		//console.log(res);
 		expect(res.days).toBe(31);
 		expect(res.hours).toBe(0);
 	});
@@ -33,9 +46,7 @@ describe('getDifference', () => {
 	it('1/7 - 31-6 Should Return 1 days and 0 hours', function() {
 		var mehr = Mds.PersianDateTime.fromPersianDate(1398, 7, 1);
 		let shahrivar = Mds.PersianDateTime.fromPersianDate(1398, 6, 31);
-
 		let res: Mds.PersianDateTimeSpan2 = mehr.getDifference(shahrivar);
-		//console.log(res);
 		expect(res.days).toBe(1);
 		expect(res.hours).toBe(0);
 	});
@@ -43,9 +54,7 @@ describe('getDifference', () => {
 	it('1/1 - 1-12 Should Return 29 days and 0 hours', function() {
 		var farvardin = new Mds.PersianDateTime(new Date('3/21/2019'));
 		let esfand = new Mds.PersianDateTime(new Date('2/20/2019'));
-
 		let res: Mds.PersianDateTimeSpan2 = farvardin.getDifference(esfand);
-		//console.log(res);
 		expect(res.days).toBe(29);
 		expect(res.hours).toBe(0);
 	});
@@ -55,7 +64,6 @@ describe('getDifference', () => {
 describe('getLongNumber', () => {
 	it('getLongNumber should return correct time', function() {
 		var persianDateTime = Mds.PersianDateTime.fromPersianDateTime(1398, 2, 1, 22, 14, 30, 0)
-		const persianDateTimeLongNumber = persianDateTime.getLongNumber();
-		expect(persianDateTimeLongNumber).toBe(13980201221430);
+		expect(persianDateTime.getLongNumber()).toBe(13980201221430);
 	});
 });
